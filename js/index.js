8fa2f256b20443837b214de71a8d555d9d8106ec
ocawebtech <ocawebtech@gmail.com>
2024-03-30 07:50:41 +0100

    Content modified

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+const menu = document.querySelector(".menu");
+const menuButtons = menu.querySelectorAll("a");
+
+menuButtons.forEach((menuBtn) => {
+  menuBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    menuButtons.forEach((btn) => btn.classList.remove("active"));
+    menuBtn.classList.add("active");
+    location.href = e.target.href;
+    hideMenu();
+  });
+});
+
+function hideMenu() {
+  const menuBTn = document.querySelector("#menu-btn");
+  menuBTn.checked = false;
+}
