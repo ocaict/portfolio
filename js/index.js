243d7c1800fbe97c1a1fd09efa3fb3579175fb41
ocawebtech <ocawebtech@gmail.com>
2024-03-31 10:18:27 +0100

    Contents Modified

--- js/index.js
+++ js/index.js
@@ -15,3 +15,45 @@ function hideMenu() {
   const menuBTn = document.querySelector("#menu-btn");
   menuBTn.checked = false;
 }
+
+const form = document.querySelector(".form");
+const sendMessage = () => {};
+
+const sanitizeMessage = (message) => {
+  const htmlRegex = /<[^>]*>/g;
+  return message.replace(htmlRegex, "");
+};
+const clearInputs = (inputs) => {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+};
+
+form.addEventListener("submit", async (e) => {
+  const url = "http://localhost:3600/contact";
+  e.preventDefault();
+  const formInputs = Array.from(form.querySelectorAll("input"));
+  const messageInput = form.querySelector("textarea");
+  formInputs.push(messageInput);
+
+  const formData = {};
+  formInputs.forEach((input) => {
+    formData[input.name] = input.value;
+  });
+
+  formData.message = sanitizeMessage(formData.message);
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (!res.ok) {
+    throw new Error("Server not Reached");
+  }
+  const result = await res.json();
+  clearInputs(formInputs);
+  console.log(result);
+});
